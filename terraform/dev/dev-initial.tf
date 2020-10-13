4cc7331a95c6c27e137f214a80f7dabc86381ead
cat-bro <cathjb@gmail.com>
2020-10-13 23:09:29 +1100

    add one-off for rabbitmq, import dev secgroups and copy for staging

--- terraform/dev/dev-initial.tf
+++ terraform/dev/dev-initial.tf
@@ -1,47 +1,47 @@
 # dev database
 resource "openstack_compute_instance_v2" "dev-db" {
   name            = "dev-db"
-  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64 [v3]"
   flavor_name     = "m3.small"
   key_pair        = "galaxy-australia"
-  security_groups = ["SSH", "galaxy-dev-db"]
+  security_groups = ["SSH", "${openstack_networking_secgroup_v2.galaxy-dev-db.name}"]
   availability_zone = "melbourne-qh2"
 }
 
 # application server / web server
 resource "openstack_compute_instance_v2" "dev" {
   name            = "dev"
-  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64 [v3]"
   flavor_name     = "m3.medium"
   key_pair        = "galaxy-australia"
-  security_groups = ["SSH", "Web-Services", "galaxy-dev", "galaxy-dev-db"]
+  security_groups = ["SSH", "Web-Services", "${openstack_networking_secgroup_v2.galaxy-dev.name}", "${openstack_networking_secgroup_v2.galaxy-dev-db.name}"]
   availability_zone = "melbourne-qh2"
 }
 
 # slurm / rabbitMQ
 resource "openstack_compute_instance_v2" "dev-queue" {
   name            = "dev-queue"
-  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64 [v3]"
   flavor_name     = "m3.small"
   key_pair        = "galaxy-australia"
-  security_groups = ["SSH", "Web-Services", "rabbitmq", "galaxy-dev"]
+  security_groups = ["SSH", "Web-Services", "rabbitmq", "${openstack_networking_secgroup_v2.galaxy-dev.name}"]
   availability_zone = "melbourne-qh2"
 }
 
 # slurm worker
 resource "openstack_compute_instance_v2" "dev-w1" {
   name            = "dev-w1"
-  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64 [v3]"
   flavor_name     = "m3.medium"
   key_pair        = "galaxy-australia"
-  security_groups = ["SSH", "galaxy-dev"]
+  security_groups = ["SSH", "${openstack_networking_secgroup_v2.galaxy-dev.name}"]
   availability_zone = "melbourne-qh2"
 }
 
 #pulsar test server
 resource "openstack_compute_instance_v2" "dev-pulsar" {
   name            = "dev-pulsar"
-  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+  image_name      = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64 [v3]"
   flavor_name     = "m3.medium"
   key_pair        = "galaxy-australia"
   security_groups = ["SSH"]
