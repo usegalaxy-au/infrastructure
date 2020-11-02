e0ef5b7aae46940ddc4d30c169f90c2198186364
cat-bro <cathjb@gmail.com>
2020-11-03 10:22:02 +1100

    need to use user_email as argument instead of user so that it does not break for anonymous users

--- files/galaxy/dynamic_job_rules/dev/dynamic_rules/destination_mapper.py
+++ files/galaxy/dynamic_job_rules/dev/dynamic_rules/destination_mapper.py
@@ -11,13 +11,15 @@ from random import sample
 TOOL_DESTINATION_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tool_destinations.yml')
 
 
-def gateway(job, app, tool, user):
+def gateway(job, app, tool, user_email):
     """
     Function to specify the destination for a job.  At present this is exactly the same
     as using dynamic_dtd with tool_destinations.yml but can be extended to more complex
     mapping such as limiting resources based on user group or selecting destinations
-    based on queue size
+    based on queue size.
+    Arguments to this function can include app, job, job_id, job_wrapper, tool, tool_id,
+    user, user_email (see https://docs.galaxyproject.org/en/latest/admin/jobs.html)
     """
-    destination = map_tool_to_destination(job, app, tool, user.email, path=TOOL_DESTINATION_PATH)
+    destination = map_tool_to_destination(job, app, tool, user_email, path=TOOL_DESTINATION_PATH)
     return destination
 
