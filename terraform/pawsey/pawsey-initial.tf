bf067f07f6dd8119c0ad04f5d2dbeeb525e8f55c
slugger70 <slugger70@gmail.com>
2020-11-09 09:02:35 +1100

    Added db volume

--- terraform/pawsey/pawsey-initial.tf
+++ terraform/pawsey/pawsey-initial.tf
@@ -138,16 +138,16 @@ resource "openstack_compute_instance_v2" "pawsey-w6" {
   }
 }
 
-## Volume for application/web server
-#resource "openstack_blockstorage_volume_v2" "pawsey-volume" {
-#  availability_zone = "nova"
-#  name        = "pawsey-volume"
-#  description = "Galaxy Australia Pawsey volume"
-#  size        = 1200
-#}
-#
-## Attachment between application/web server and volume
-#resource "openstack_compute_volume_attach_v2" "attach-pawsey-volume-to-pawsey" {
-#  instance_id = "${openstack_compute_instance_v2.pawsey.id}"
-#  volume_id   = "${openstack_blockstorage_volume_v2.pawsey-volume.id}"
-#}
+# Volume for database server
+resource "openstack_blockstorage_volume_v2" "pawsey-db-volume" {
+ availability_zone = "nova"
+ name        = "pawsey-db-volume"
+ description = "Galaxy Australia Pawsey database volume"
+ size        = 200
+}
+
+# Attachment between application/web server and volume
+resource "openstack_compute_volume_attach_v2" "attach-pawsey-db-volume-to-pawsey-db" {
+ instance_id = "${openstack_compute_instance_v2.pawsey-db.id}"
+ volume_id   = "${openstack_blockstorage_volume_v2.pawsey-db-volume.id}"
+}
