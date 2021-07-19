d7bf147dbc50b8d7b4ece36278fa7d6441eda6d3
cat-bro <cathjb@gmail.com>
2021-07-19 17:41:06 +1000

    update builtin dependencies script for bioblend 0.16.0

--- scripts/install_builtin_dependencies.py
+++ scripts/install_builtin_dependencies.py
@@ -4,8 +4,7 @@ import time
 import datetime as dt
 
 from bioblend import ConnectionError
-from bioblend.galaxy import GalaxyInstance as bioblend_GalaxyInstance
-from bioblend.galaxy.client import Client
+from bioblend.galaxy import GalaxyInstance
 
 install_timeout = 600
 sleep_interval = 10
@@ -14,6 +13,8 @@ sleep_interval = 10
 Install unresolved conda environments on a Galaxy instance.  This can be run at any time but its purpose
 is to set up environments for galaxy built-in tools on a new instance.  By default resolve only builtin
 environments but set --all to include environments for toolshed tools
+
+requires bioblend>=0.16.0
 """
 
 def main():
@@ -58,93 +59,12 @@ def main():
 
 
 def dependency_is_resolved(galaxy_instance, tool_id):
-    [resolver_info] = galaxy_instance.tool_dependencies.summarize_toolbox(index_by='tool', tool_ids=[tool_id])
+    [resolver_info] = galaxy_instance.tool_dependencies.summarize_toolbox(index_by='tools', tool_ids=[tool_id])
     return 'NullDependency' not in [s['model_class'] for s in resolver_info['status']]
 
 
 def user_is_admin(galaxy_instance):
     return galaxy_instance.config.get_config().get('is_admin_user', False)
-
-
-class ToolDependenciesClient(Client):  # TODO: try getting this into bioblend
-
-    def __init__(self, galaxy_instance):
-        self.module = 'dependency_resolvers'
-        super().__init__(galaxy_instance)
-
-    def summarize_toolbox(self, index=None, tool_ids=[], resolver_type=None, include_containers=None, container_type=None, index_by=None):
-        """
-        GET /api/dependency_resolvers/toolbox
-
-        Summarize requirements across toolbox (for Tool Management grid). This is an experiemental
-        API particularly tied to the GUI - expect breaking changes until this notice is removed.
-
-        :type   index:    int
-        :param  index:    index of the dependency resolver
-        :type   tool_ids: list
-        :param  tool_ids: tool_ids to return when index_by=tool
-        :type   resolver_type:  str
-        :param  resolver_type:  restrict to specified resolver type
-        :type   include_containers: bool
-        :param  include_containers: include container resolvers in resolution
-        :type   container_type: str
-        :param  container_type: restrict to specified container type
-        :type   index_by: str
-        :param  index_by: By default consider only context of requirements, group tools by requirements.
-                          Set this to 'tools' to summarize across all tools though. Tools may provide additional
-                          context for container resolution for instance.
-
-        :rtype:     list
-        :returns:   dictified descriptions of the dependencies, with attribute
-            `dependency_type: None` if no match was found.
-            For example::
-        
-            [{'requirements': [{'name': 'galaxy_sequence_utils',
-                                'specs': [],
-                                'type': 'package',
-                                'version': '1.1.4'},
-                               {'name': 'bx-python',
-                                'specs': [],
-                                'type': 'package',
-                                'version': '0.8.6'}],
-              'status': [{'cacheable': False,
-                          'dependency_type': None,
-                          'exact': True,
-                          'model_class': 'NullDependency',
-                          'name': 'galaxy_sequence_utils',
-                          'version': '1.1.4'},
-                          {'cacheable': False,
-                          'dependency_type': None,
-                          'exact': True,
-                          'model_class': 'NullDependency',
-                          'name': 'bx-python',
-                          'version': '0.8.6'}],
-              'tool_ids': ['vcf_to_maf_customtrack1']}]        
-        """
-
-        params = {}
-        if index:
-            params['index'] = str(index)
-        if tool_ids:
-            params['tool_ids'] = ','.join(tool_ids)
-        if resolver_type:
-            params['resolver_type'] = resolver_type
-        if include_containers is not None:
-            params['include_containers'] = str(include_containers)
-        if container_type:
-            params['container_type'] = container_type
-        if index_by:
-            params['index_by'] = index_by
-
-        url = self._make_url() + '/toolbox'
-        return self._get(url=url, params=params)
-
-
-class GalaxyInstance(bioblend_GalaxyInstance):
-
-    def __init__(self, url, key=None, **kwargs):
-        super().__init__(url, key, **kwargs)
-        self.tool_dependencies = ToolDependenciesClient(self)
 
 
 if __name__ == '__main__':
