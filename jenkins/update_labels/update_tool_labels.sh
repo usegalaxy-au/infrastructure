4a7e2cea85631ba7f27a54d56cf6ca2693d1555b
cat-bro <cathjb@gmail.com>
2021-08-24 07:44:11 +1000

    update update_labels

--- jenkins/update_labels/update_tool_labels.sh
+++ jenkins/update_labels/update_tool_labels.sh
@@ -19,4 +19,4 @@ echo "secret" > .vault_pass.txt  # ansible needs .vault_pass.txt to exist
 
 ansible-playbook -i hosts jenkins/update_labels/update_tool_labels_playbook.yml --extra-vars "ansible_user=jenkins_bot update_labels_hostname=staging_galaxy_server"
 
-ansible-playbook -i hosts jenkins/update_labels/update_tool_labels_playbook.yml --extra-vars "ansible_user=jenkins_bot update_labels_hostname=pawsey_galaxy_server"
+ansible-playbook -i hosts jenkins/update_labels/update_tool_labels_playbook.yml --extra-vars "ansible_user=jenkins_bot update_labels_hostname=aarnet_galaxy_server"
