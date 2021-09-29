8d9d7af421e95c0c45c662745cfefd6617a35669
Nick-Rhodes-QFAB <n.rhodes@qcif.edu.au>
2021-09-29 21:38:08 +1000

    Changes to volume addition

--- terraform/high-mem-qld/pulsar-high-mem-qld.tf
+++ terraform/high-mem-qld/pulsar-high-mem-qld.tf
@@ -1,10 +1,11 @@
 locals {
   worker_count = 3
-  tmp_disk_size = 10000
+  tmp_disk_size = 100
   avail_zone = "QRIScloud"
   key_pair = "galaxy-australia"
   image_name = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
-  flavor_name  = "qld.biocommons.240c4000g"
+#  flavor_name  = "qld.biocommons.240c4000g"
+  flavor_name = "qld.biocommons.8c24g"
 
   #Loops for worker nodes
   instances = toset(formatlist("%d", range(local.worker_count)))
@@ -28,4 +29,28 @@ resource "openstack_compute_instance_v2" "himem-nodes"{
   key_pair        = local.key_pair
   security_groups = ["SSH only", "default"]
   availability_zone = local.avail_zone
+}
+
+######################################################################################
+# Volumes
+######################################################################################
+
+# himem temp volumes
+resource "openstack_blockstorage_volume_v2" "himem_tmp_disk" {
+  for_each = local.volumes
+  availability_zone = local.avail_zone
+  name        = each.value
+  description = "himem tmp disk"
+  size        = local.tmp_disk_size
+}
+
+######################################################################################
+# Attachments
+######################################################################################
+
+# Attachment between nodes and himem temp volumes
+resource "openstack_compute_volume_attach_v2" "attach-himem-tmp-volume-to-himem" {
+  for_each = { for idx in local.attachments: idx.instance => idx }
+  instance_id = openstack_compute_instance_v2.himem-nodes[each.value.instance].id
+  volume_id   = openstack_blockstorage_volume_v2.himem_tmp_disk[each.value.volume].id
 }
