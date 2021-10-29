6d0a2ac4044f0db2f87431c1d1fc836043f92117
cat-bro <cathjb@gmail.com>
2021-10-29 17:29:45 +1100

    aaarh

--- scripts/convert_dtd.py
+++ scripts/convert_dtd.py
@@ -2,7 +2,8 @@ import oyaml as yaml
 import re
 from bioblend.galaxy import GalaxyInstance
 
-local_tools_not_in_panel = ['upload1']
+local_tools_not_in_panel = ['upload1', 'Extract genomic DNA 1']  # galaxy built-in tools that are not returned by the get_tools() function
+oddly_named_data_managers = ['kraken2_build_database']  # data manager tool short ids that do not contain the substring 'data_manager'
 
 # todo: the gffread rule will not translate well
 
@@ -31,8 +32,19 @@ def make_lower_upper_expression(lower, upper):
     else:
         return f'input_size >= {lower} and input_size < {upper}'
 
-ntasks_pattern = re.compile('.*--ntasks=(?P<ntasks>\d+).*')
-mem_pattern = re.compile('.*--mem=(?P<mem>\d+).*')
+def get_cores_from_env(env):
+    ntasks_pattern = re.compile('.*--ntasks=(?P<ntasks>\d+).*')
+    native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
+    ntasks = int(re.match(ntasks_pattern, native_spec).groupdict().get('ntasks'))
+    return ntasks
+
+def get_mem_from_env(env):
+    print(env)
+    mem_pattern = re.compile('.*--mem=(?P<mem>\d+).*')
+    native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
+    mem = int(re.match(mem_pattern, native_spec).groupdict().get('mem'))
+    return mem
+
 
 galaxy_instance = GalaxyInstance('https://usegalaxy.org.au')
 
@@ -42,19 +54,20 @@ tool_destinations_file = '/Users/cat/dev/infrastructure/files/galaxy/dynamic_job
 
 job_config_file = '/Users/cat/dev/infrastructure/templates/galaxy/config/pawsey_job_conf.yml.j2'
 
-destination_cores = {}
+# destination_cores = {}
 
 with open(job_config_file) as handle:
-    envs = yaml.safe_load(handle).get('execution').get('environments')
+    job_conf = yaml.safe_load(handle)
+    envs = job_conf.get('execution').get('environments')
 
-for env_id in envs:
-    env = envs[env_id]
-    native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
-    if native_spec:
-        ntasks = int(re.match(ntasks_pattern, native_spec).groupdict().get('ntasks'))
-        destination_cores[env_id] = ntasks
+# for env_id in envs:
+#     env = envs[env_id]
+#     native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
+#     if native_spec:
+#         ntasks = int(re.match(ntasks_pattern, native_spec).groupdict().get('ntasks'))
+#         destination_cores[env_id] = ntasks
 
-destination_cores['fail'] = 'fail'
+# destination_cores['fail'] = 'fail'
 
 with open(tool_destinations_file) as handle:
     tool_dests = yaml.safe_load(handle)['tools']
@@ -75,7 +88,8 @@ for tool_id in tool_dests:
             if rule['destination'] == 'fail':
                 new_rule.update({'fail': rule['fail_message']})
             else:
-                new_rule['cores'] = destination_cores[rule['destination']]            
+                new_rule['cores'] = get_cores_from_env(envs[rule['destination']])           
+                new_rule['mem'] = get_mem_from_env(envs[rule['destination']])
             file_size_rules.append(new_rule)
         allowed_dests.append(rule['destination'])
     if any([x.startswith('pulsar') for x in allowed_dests]):  # careful: there are some blast rules that go pulsar or slurm depending on params
@@ -84,7 +98,8 @@ for tool_id in tool_dests:
         file_size_rules.append({
             'lower': '0',
             'upper': 'Infinity',
-            'cores': destination_cores[tool['default_destination']]
+            'cores': get_cores_from_env(envs[tool['default_destination']]),
+            'mem': get_mem_from_env(envs[tool['default_destination']]),
         })
     # now order them from lowest to highest.  Assume they make sense.
     file_size_rules = sorted(file_size_rules, key = lambda x: 10**9 if x['upper'] == 'Infinity' else eval(x['upper']))
@@ -92,14 +107,16 @@ for tool_id in tool_dests:
         lowest_rule = {
             'lower': '0',
             'upper': file_size_rules[0]['lower'],
-            'cores': destination_cores[tool['default_destination']]            
+            'cores': get_cores_from_env(envs[tool['default_destination']]),
+            'mem': get_mem_from_env(envs[tool['default_destination']]),           
         }
         file_size_rules = [lowest_rule] + file_size_rules
     if file_size_rules and not any([x['upper'] == 'Infinity' for x in file_size_rules]):
         highest_rule = {
             'lower': file_size_rules[-1]['upper'],
             'upper': 'Infinity',
-            'cores': destination_cores[tool['default_destination']]            
+            'cores': get_cores_from_env(envs[tool['default_destination']]),
+            'mem': get_mem_from_env(envs[tool['default_destination']]),            
         }
         file_size_rules.append(highest_rule)
     vortex_tool = {}
@@ -125,7 +142,8 @@ for tool_id in tool_dests:
     ]))
     matching_galaxy_local_tool_id_regexes = [id for id in galaxy_tool_ids if id == tool_id] # i.e. exact match regex
     included_unmatched_tools = [id for id in local_tools_not_in_panel if id == tool_id] # i.e. exact match regex
-    new_ids = matching_galaxy_toolshed_tool_id_regexes + matching_galaxy_local_tool_id_regexes
+    data_managers = [f'.*{tool_id}.*'] if 'data_manager' in tool_id or tool_id in oddly_named_data_managers else []
+    new_ids = matching_galaxy_toolshed_tool_id_regexes + matching_galaxy_local_tool_id_regexes + included_unmatched_tools + data_managers
     if len(new_ids) == 0:
         print(f'No galaxy tool found for short_id {tool_id}')
     if len(new_ids) > 1:
@@ -137,14 +155,40 @@ with open('vortex_tools.yml', 'w') as handle:
     yaml.dump({'tools': vortex_tools}, handle)
 
 # now for destinations
+
+# split destinations by runners IF they destinations have native specifications 
 destinations = {}
+# print(destination_cores.keys())
+# new_env_ids = set(list(['_'.join(env_id.split('_')[:-1]) for env_id in destination_cores.keys()]))
+# print(new_env_ids)
 
-new_env_ids = set(list([env_id.split('_')[:-1] for env_id in destination_cores]))
+new_dest_ids = []
+for runner_id in job_conf['runners'].keys():
+    runner_envs = [env_id for env_id in envs if envs[env_id]['runner'] == runner_id]
+    if len(runner_envs) == 0:
+        continue
+    if len(runner_envs = 1):
+        new_dest_id = runner_envs[0]
+    else:
+        e0, e1 = runner_envs[:2]
+        longest_prefix_length = max([x for x in range(min(len(e0), len(e1))) if e0[:x] == e1[:x]])
+        new_dest_id = e0[:longest_prefix_length]
+    sorted_env_ids = sorted(runner_envs, key=lambda x: get_cores_from_env(envs[x]), reverse=True)
+    new_dest = envs[sorted_env_ids[0]].copy()
+    destinations[new_dest_id] = new_dest
 
-for new_env_id in new_env_ids:
-    max_existing_env = sorted([e for e in destination_cores if e.startwith(new_id)], reverse=True)[0]
-    new_env = max_existing_env.copy()
-    destinations[new_env_id] = new_env
+# vortex_destinations
+# for dest in destionations
+
+
+
+
+
+# for new_env_id in new_env_ids:
+#     print(new_env_id)
+#     max_existing_env = sorted([e for e in destination_cores.keys() if e.startswith(new_id)], reverse=True)[0]
+#     new_env = max_existing_env.copy()
+#     destinations[new_env_id] = new_env
 
 with open('destinations_for_job_conf.yml', 'w') as handle:
     yaml.dump({'environments': destinations}, handle)
@@ -152,9 +196,9 @@ with open('destinations_for_job_conf.yml', 'w') as handle:
 vortex_destinations = {}
 for id in destinations:
     env = destinations[id]
-    native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
-    ntasks = int(re.match(ntasks_pattern, native_spec).groupdict().get('ntasks'))
-    mem = int(re.match(mem_pattern, native_spec).groupdict().get('mem'))
+    # native_spec = env.get('nativeSpecification') or env.get('submit_native_specification')
+    ntasks = get_cores_from_env(env)
+    mem = get_mem_from_env(env)
     vortex_dest = {'cores': ntasks, 'mem': mem/1024, 'scheduling': {}}
     vortex_dest['scheduling']['allow'] = [id]  # each destination gets its own tag
     if id.startswith('pulsar'):
