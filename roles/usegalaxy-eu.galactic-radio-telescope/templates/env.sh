e047415270639a5b6c163f8f09eb416003d55a2a
cat-bro <cathjb@gmail.com>
2022-03-03 15:20:55 +1100

    grt playbook, terraform, variables, nginx config etc

--- roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
+++ roles/usegalaxy-eu.galactic-radio-telescope/templates/env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export DJANGO_SETTINGS_MODULE=base.production
-export DJANGO_ALLOWED_HOSTS="{{ grt_allowed_hosts }}"
-export GRT_UPLOAD_DIR={{ grt_upload_dir }}
-export PGHOST="{{ grt_pghost }}" PGUSER="{{ grt_pguser }}" PGNAME="{{ grt_pgname }}" PGPORT="{{ grt_pgport }}" PGPASSWORD="{{ grt_pgpassword }}"
+export DJANGO_SETTINGS_MODULE=base.production \
+    DJANGO_ALLOWED_HOSTS="{{ grt_allowed_hosts }}" \
+    GRT_UPLOAD_DIR={{ grt_upload_dir }} \
+    PGUSER="{{ grt_pguser }}" \
+    PGNAME="{{ grt_pgname }}" \
+    PGPASSWORD="{{ grt_pgpassword }}" 
+    PGHOST="{{ grt_pghost }}" \
+    PGPORT="{{ grt_pgport }}" \
+    DJANGO_DEBUG="{{ grt_django_debug }}"  # do not set to true on production
