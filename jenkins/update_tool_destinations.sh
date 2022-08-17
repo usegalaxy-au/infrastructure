8997f3714c2eaf4cabec978f535db98bbe480ca2
cat-bro <cathjb@gmail.com>
2022-08-17 11:33:25 +1000

    Use common function to activate virtualenv

--- jenkins/update_tool_destinations.sh
+++ jenkins/update_tool_destinations.sh
@@ -1,3 +1,5 @@
+source jenkins/utils.sh
+
 # check whether the production tpv files have changed
 PROD_TPV_FILE_PATH="files/galaxy/dynamic_job_rules/production/total_perspective_vortex"
 PROD_TPV_UPDATED=$(git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | cat | grep $PROD_TPV_FILE_PATH)
@@ -20,24 +22,8 @@ if [ ! "$PROD_TPV_UPDATED" ] && [ ! "$DEV_RELEVANT_FILES_UPDATED" ] && [ ! "$STA
     exit 0
 fi
 
-# Activate the virtual environment on jenkins. If this script is being run for
-# the first time we will need to set up the virtual environment
-VENV_DIR="/var/lib/jenkins/jobs_common"
-VIRTUALENV="$VENV_DIR/.ansible_venv"
-REQUIREMENTS_FILE="jenkins/requirements.txt"
-CACHED_REQUIREMENTS_FILE="$VIRTUALENV/cached_requirements.txt"
+activate_virtualenv
 echo "helloworld" > .vault_pass.txt  # ansible needs .vault_pass.txt to exist
-
-[ ! -d $VIRTUALENV ] && virtualenv -p python3 $VIRTUALENV
-# shellcheck source=../.venv3/bin/activate
-. "$VIRTUALENV/bin/activate"
-
-# if requirements change, reinstall requirements
-[ ! -f $CACHED_REQUIREMENTS_FILE ] && touch $CACHED_REQUIREMENTS_FILE
-if [ "$(diff $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE)" ]; then
-pip install -r $REQUIREMENTS_FILE
-cp $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE
-fi
 
 if [ "$PROD_TPV_UPDATED" ]; then
     echo -e "\nUpdating tool destinations file on Galaxy production instance\n"
---
+++ jenkins/utils.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+activate_virtualenv() {
+    # Activate the virtual environment on jenkins. If this script is being run for
+    # the first time we will need to set up the virtual environment
+    VENV_DIR="/var/lib/jenkins/jobs_common"
+    VIRTUALENV="$VENV_DIR/.ansible_venv"
+    REQUIREMENTS_FILE="jenkins/requirements.txt"
+    CACHED_REQUIREMENTS_FILE="$VIRTUALENV/cached_requirements.txt"
+
+    [ ! -d $VIRTUALENV ] && virtualenv -p python3 $VIRTUALENV
+    # shellcheck source=../.venv3/bin/activate
+    . "$VIRTUALENV/bin/activate"
+
+    # if requirements change, reinstall requirements
+    [ ! -f $CACHED_REQUIREMENTS_FILE ] && touch $CACHED_REQUIREMENTS_FILE
+    if [ "$(diff $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE)" ]; then
+        pip install -r $REQUIREMENTS_FILE
+        cp $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE
+    fi
+}
