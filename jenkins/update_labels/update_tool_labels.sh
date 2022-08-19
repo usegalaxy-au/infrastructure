7fab86bcaa961e115abb0d06979b0eee89894b57
cat-bro <cathjb@gmail.com>
2022-08-19 11:38:58 +1000

    Common ansible env for jenkins jobs, add config_backup playbook (#932)
    * Move .ansible_venv to ~/jobs_common on jenkins VM and update version
    
    * Use common function to activate virtualenv
    
    * shebangs n whitespace
    
    * config backup wip
    
    * fix paths and things
    
    * no need to gitignore

--- jenkins/update_labels/update_tool_labels.sh
+++ jenkins/update_labels/update_tool_labels.sh
@@ -1,20 +1,8 @@
-# Activate the virtual environment on jenkins. If this script is being run for
-# the first time we will need to set up the virtual environment
-VIRTUALENV="../.ansible_venv"
-REQUIREMENTS_FILE="jenkins/requirements.txt"
-CACHED_REQUIREMENTS_FILE="$VIRTUALENV/cached_requirements.txt"
+#! /bin/bash
 
-[ ! -d $VIRTUALENV ] && virtualenv -p python3 $VIRTUALENV
-# shellcheck source=../.venv3/bin/activate
-. "$VIRTUALENV/bin/activate"
+source jenkins/utils.sh
 
-# if requirements change, reinstall requirements
-[ ! -f $CACHED_REQUIREMENTS_FILE ] && touch $CACHED_REQUIREMENTS_FILE
-if [ "$(diff $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE)" ]; then
-pip install -r $REQUIREMENTS_FILE
-cp $REQUIREMENTS_FILE $CACHED_REQUIREMENTS_FILE
-fi
-
+activate_virtualenv
 echo "secret" > .vault_pass.txt  # ansible needs .vault_pass.txt to exist
 
 ansible-playbook -i hosts jenkins/update_labels/update_tool_labels_playbook.yml --extra-vars "ansible_user=jenkins_bot update_labels_hostname=staging_galaxy_server"
