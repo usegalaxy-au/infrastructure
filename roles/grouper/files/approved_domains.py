3af6aad5cf33b9276dc00f96d979b3fbc8fbcc32
cat-bro <cathjb@gmail.com>
2025-12-10 13:46:25 +1100

    Update approved_domains based on Galaxy group member info

--- roles/grouper/files/approved_domains.py
+++ roles/grouper/files/approved_domains.py
@@ -1,21 +1,50 @@
 approved_domains = {
+  "AGRF": [
+    "agrf.org.au"
+  ],
   "AdelaideU_users": [
     "adelaide.edu.au",
     "student.adelaide.edu.au"
   ],
+  "Australian National University": [
+    "anu.edu.au"
+  ],
+  "Australian_Museum": [
+    "australian.museum"
+  ],
+  "Australian_Red_Cross": [
+    "redcrossblood.org.au"
+  ],
   "Australian_government": [
+    "aad.gov.au",
     "act.gov.au",
+    "aff.gov.au",
+    "afp.gov.au",
     "agriculture.gov.au",
     "agriculture.vic.gov.au",
     "aims.gov.au",
+    "ansto.gov.au",
+    "antarctica.gov.au",
     "austmus.gov.au",
+    "awe.gov.au",
+    "botanicgardens.nsw.gov.au",
     "daf.qld.gov.au",
     "dbca.wa.gov.au",
+    "dcceew.gov.au",
+    "deeca.vic.gov.au",
     "defence.gov.au",
     "des.qld.gov.au",
+    "detsi.qld.gov.au",
+    "dhhs.vic.gov.au",
     "dpi.nsw.gov.au",
+    "dpi.qld.gov.au",
+    "dpie.nsw.gov.au",
+    "dpipwe.tas.gov.au",
+    "dpird.wa.gov.au",
+    "dst.defence.gov.au",
     "ecodev.vic.gov.au",
     "education.nsw.gov.au",
+    "education.vic.gov.au",
     "environment.gov.au",
     "foodstandards.gov.au",
     "health.gov.au",
@@ -23,102 +52,119 @@ approved_domains = {
     "health.qld.gov.au",
     "health.wa.gov.au",
     "hnehealth.nsw.gov.au",
+    "industry.gov.au",
+    "justice.vic.gov.au",
+    "measurement.gov.au",
+    "museum.vic.gov.au",
+    "museum.wa.gov.au",
+    "nre.tas.gov.au",
+    "nt.gov.au",
+    "phcn.vic.gov.au",
     "qm.qld.gov.au",
     "rbg.vic.gov.au",
     "sa.gov.au",
-    "ths.tas.gov.au"
+    "ths.tas.gov.au",
+    "zoo.nsw.gov.au"
   ],
   "Australian_other": [
+    "accugen.com.au",
+    "acelabservices.com.au",
+    "apiam.com.au",
+    "autismcanberra.com.au",
+    "awri.com.au",
+    "baiada.com.au",
     "baulkhamhillshighschool.com.au",
     "bioproperties.com.au",
+    "birlinglabs.com.au",
     "bri.sonichealth.com.au",
+    "busseltonjetty.com.au",
     "crocresearch.com.au",
+    "decodecience.com.au",
+    "dhm.com.au",
     "genea.com.au",
+    "genepathlabs.com.au",
+    "geneseq.com.au",
+    "geneworks.com.au",
+    "genics.com.au",
+    "genomicsforlife.com.au",
+    "healius.com.au",
+    "hri.com.au",
     "inaburrastudents.nsw.edu.au",
+    "inertech.com.au",
+    "ithinkmarketing.com.au",
+    "laverty.com.au",
     "lonsbio.com.au",
     "microbelabs.com.au",
+    "pcgenetics.com.au",
+    "pestkill.com.au",
     "rcpaqap.com.au",
+    "redboxresearchdata.com.au",
     "snp.com.au",
+    "southernrna.com.au",
+    "speedx.com.au",
     "sugarresearch.com.au",
     "tasalk.com.au",
-    "uniquest.com.au"
+    "testneeds.com.au",
+    "tigs.com.au",
+    "uniquest.com.au",
+    "westbournecollege.com.au"
   ],
   "Australian_researchers_mix": [
-    "CDU.EDU.AU",
     "aapoly.edu.au",
     "aarnet.edu.au",
-    "agrf.org.au",
-    "anu.edu.au",
-    "bakeridi.edu.au",
+    "austin.org.au",
     "biocommons.org.au",
     "bioplatforms.com",
-    "bond.edu.au",
-    "calvarymater.org.au",
-    "ccg.murdoch.edu.au",
     "ccia.org.au",
-    "cdu.edu.au",
-    "centenary.org.au",
-    "cmri.org.au",
-    "cqu.edu.au",
-    "csu.edu.au",
-    "deakin.edu.au",
-    "ecu.edu.au",
-    "federation.edu.au",
-    "flinders.edu.au",
-    "florey.edu.au",
-    "garvan.org.au",
     "gebri.usc.edu.eg",
-    "hdr.mq.edu.au",
-    "hudson.org.au",
-    "inaburrastudents.nsw.edu.au",
-    "latrobe.edu.au",
-    "mater.org.au",
-    "mcri.edu.au",
-    "menzies.edu.au",
-    "mh.org.au",
-    "mq.edu.au",
-    "murdoch.edu.au",
-    "mymail.unisa.edu.au",
-    "newcastle.edu.au",
-    "onjcri.org.au",
+    "microbialscreening.com",
+    "myco.org.au",
     "parrastu.catholic.edu.au",
     "pawsey.org.au",
-    "petermac.org",
-    "qcif.edu.au",
     "qfab.org",
-    "qimr.edu.au",
-    "qimrberghofer.edu.au",
-    "redcrossblood.org.au",
-    "research.usc.edu.au",
-    "sahmri.com",
-    "scu.edu.au",
     "sims.org.au",
     "smu.org.au",
     "student.edu.au",
     "student.kings.edu.au",
-    "student.murdoch.edu.au",
-    "student.scu.edu.au",
-    "student.swin.edu.au",
-    "student.usc.edu.au",
-    "student.westernsydney.edu.au",
-    "students.latrobe.edu.au",
-    "students.mq.edu.au",
-    "svha.org.au",
-    "svi.edu.au",
-    "swin.edu.au",
-    "telethonkids.org.au",
-    "une.edu.au",
-    "unisa.edu.au",
-    "uon.edu.au",
-    "uow.edu.au",
-    "usc.edu.au",
+    "students.aih.edu.au",
     "vcgs.org.au",
-    "vidrl.org.au",
-    "wehi.edu.au",
-    "westernsydney.edu.au"
+    "victorchang.edu.au",
+    "vidrl.org.au"
+  ],
+  "Baker_Heart_and_Diabetes_Institute": [
+    "baker.edu.au",
+    "bakeridi.edu.au"
+  ],
+  "Bond_University": [
+    "bond.edu.au",
+    "student.bond.edu.au"
+  ],
+  "Burnet_Institute": [
+    "burnet.edu.au",
+    "student.burnet.edu.au"
   ],
   "CSIRO": [
     "csiro.au"
+  ],
+  "Calvary": [
+    "calvarymater.org.au"
+  ],
+  "Centenary_Institute_of_Cancer_Medicine_and_Cell_Biology": [
+    "centenary.org.au"
+  ],
+  "Central_Queensland_University": [
+    "cqu.com.au",
+    "cqu.edu.au",
+    "cqumail.com"
+  ],
+  "Charles_Darwin_University": [
+    "cdu.edu.au"
+  ],
+  "Charles_Sturt_University": [
+    "csu.edu.au"
+  ],
+  "Children's Medical Research Institute": [
+    "cmri.org.au"
   ],
   "Curtin_users": [
     "curtin.edu.au",
@@ -126,34 +172,137 @@ approved_domains = {
     "postgrad.curtin.edu.au",
     "student.curtin.edu.au"
   ],
+  "Deakin University": [
+    "deakin.edu.au",
+    "research.deakin.edu.au"
+  ],
+  "Edith_Cowan_University": [
+    "ecu.edu.au",
+    "our.ecu.edu.au"
+  ],
+  "Federation_University_Australia": [
+    "federation.edu.au",
+    "students.federation.edu.au"
+  ],
+  "Flinders University": [
+    "flinders.edu.au"
+  ],
+  "Florey_Institute_of_Neuroscience_and_Mental_Health": [
+    "florey.edu.au"
+  ],
+  "Garvan Institute of Medical Research": [
+    "garvan.org.au"
+  ],
   "GriffithU_users": [
     "griffith.edu.au",
     "griffithuni.edu.au"
   ],
+  "Hudson_Institute_of_Medical_Research": [
+    "hudson.org.au"
+  ],
   "JCU_users": [
     "jcu.edu.au",
+    "my.jcu.edu",
     "my.jcu.edu.au"
+  ],
+  "La_Trobe_University": [
+    "latrobe.edu.au",
+    "ltu.edu.au",
+    "student.latrobe.edu.au",
+    "students.latrobe.edu.au",
+    "students.ltu.edu.au"
+  ],
+  "Macquarie_University": [
+    "hdr.mq.edu.au",
+    "mq.edu.au",
+    "students.mq.edu.au"
+  ],
+  "Mater_Research": [
+    "mater.org.au"
+  ],
+  "Melbourne_Health": [
+    "mh.org.au"
+  ],
+  "Menzies_School_of_Health_Research": [
+    "menzies.edu.au"
   ],
   "Monash_users": [
     "monash.edu",
     "monash.edu.au",
     "student.monash.edu"
   ],
+  "Murdoch_Childrens_Research_Institute": [
+    "mcri.edu.au",
+    "mcri.org.au"
+  ],
+  "Murdoch_University": [
+    "ccg.murdoch.edu.au",
+    "iiid.murdoch.edu.au",
+    "murdoch.edu.au",
+    "student.murdoch.edu",
+    "student.murdoch.edu.au"
+  ],
+  "Peter_MacCallum_Cancer_Centre": [
+    "petermac.org"
+  ],
   "QUT_users": [
+    "connect.edu.qut.au",
+    "connect.qut.ed.au",
+    "connect.qut.edu",
     "connect.qut.edu.au",
     "edu.qut.au",
     "hdr.qut.edu.au",
     "qut.connect.edu.au",
     "qut.edu.au",
-    "qut.hdr.edu.au"
+    "qut.hdr.edu.au",
+    "students.qut.edu.au"
+  ],
+  "Queensland_Institute_of_Medical_Research": [
+    "qimr.edu.au",
+    "qimrberghofer.edu.au"
   ],
   "RMIT_users": [
     "rmit.edu.au",
+    "rmit.student.edu.au",
     "student.rmit.edu.au"
   ],
+  "South Australian Health and Medical Research Institute": [
+    "sahmri.com"
+  ],
+  "Southern_Cross_University": [
+    "scu.edu.au",
+    "scu.student.edu.au",
+    "student.scu.edu.au"
+  ],
+  "St_Vincent's_Institute_of_Medical_Research": [
+    "svi.edu.au"
+  ],
+  "St_Vincents_Health_Australia": [
+    "svha.org.au"
+  ],
+  "Swinburne_University_of_Technology": [
+    "student.swin.edu.au",
+    "swin.edu.au"
+  ],
+  "Telethon_Kids_Institute": [
+    "telethonkids.org.au"
+  ],
+  "The_Olivia_Newton-John_Cancer_Research_Institute": [
+    "onjcri.org.au"
+  ],
+  "The_University_of_New_England": [
+    "myune.edu.au",
+    "une.edu.au"
+  ],
+  "The_University_of_Newcastle": [
+    "newcastle.edu.au",
+    "uon.edu.au"
+  ],
   "UNSW_users": [
+    "ad.unsw.ed.au",
     "ad.unsw.edu.au",
     "ccia.unsw.edu.au",
+    "kirby.unsw.edu.au",
     "student.unsw.edu.au",
     "unsw.edu.au",
     "unsw.edu.au ",
@@ -161,10 +310,14 @@ approved_domains = {
   ],
   "UQ_users": [
     "awmc.uq.edu.au",
+    "cai.uq.edu.au",
     "imb.uq.edu.au",
     "mater.uq.edu.au",
+    "me.com",
+    "qcif.edu.au",
     "student.uq.edu.au",
     "student.uqconnect.edu.au",
+    "tri.edu.au",
     "uq.connect.edu.au",
     "uq.edu",
     "uq.edu.au",
@@ -179,6 +332,7 @@ approved_domains = {
   ],
   "USQ_users": [
     "umail.usq.edu.au",
+    "unisq.edu.au",
     "usq.edu.au"
   ],
   "UTS_users": [
@@ -186,21 +340,55 @@ approved_domains = {
     "uts.edu.au"
   ],
   "UTas_users": [
-    "utas.edu.au"
+    "utas.edu.au",
+    "utas.edua.au"
   ],
   "UWA_users": [
+    "perkins.uwa.edu.au",
+    "perron.uwa.edu.au",
+    "research.uwa.ed.au",
     "research.uwa.edu.au",
     "resphealth.uwa.edu.au",
     "student.uwa.du.au",
     "student.uwa.edu.au",
+    "student.uwa.eu.au",
     "uwa.edu.au"
   ],
+  "University of Canberra": [
+    "canberra.edu.au",
+    "uni.canberra.edu.au"
+  ],
+  "University of South Australia": [
+    "mymail.unisa.edu.au",
+    "unisa.edu.au"
+  ],
+  "University_of_Wollongong": [
+    "uow.edu.au",
+    "uowmail.edu.au"
+  ],
+  "University_of_the_Sunshine_Coast": [
+    "research.usc.edu.au",
+    "student.usc.edu.au",
+    "usc.edu.au",
+    "usc.student.edu.au"
+  ],
   "UoM_users": [
+    "student.unimelb.edu",
     "student.unimelb.edu.au",
     "unimelb.edu.au"
   ],
   "UoSydney_users": [
     "sydney.edu.au",
     "uni.sydney.edu.au"
+  ],
+  "Victoria University": [
+    "vu.edu.au"
+  ],
+  "Walter_and_Eliza_Hall_Institute": [
+    "wehi.edu.au"
+  ],
+  "Western Sydney University": [
+    "student.westernsydney.edu.au",
+    "westernsydney.edu.au"
   ]
 }
