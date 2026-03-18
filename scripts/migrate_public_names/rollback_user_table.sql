3ca68807452ab74e331281f021a04a4b329be25c
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2026-03-18 10:22:32 +0530

    Add rollback script

---
+++ scripts/migrate_public_names/rollback_user_table.sql
@@ -0,0 +1,17 @@
+-- before running, update the galaxy_user_backup_<timestamp> to the correct table
+-- Set this to 1 if you want to commit, 0 if you want a dry-run
+\set COMMIT_MODE 1
+
+BEGIN;
+UPDATE galaxy_session SET is_valid = false WHERE is_valid = true;
+UPDATE galaxy_user g
+SET username = p.new_username
+FROM galaxy_user_backup_<timestamp> b
+WHERE g.id = b.id;
+
+\if :COMMIT_MODE
+COMMIT;
+\else
+\echo 'Set COMMIT_MODE to 1 to commit changes'
+ROLLBACK;
+\endif
